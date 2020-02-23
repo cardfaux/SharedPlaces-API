882405af9ba065935654534c4c8195426ef22999
James Hagood <cardfaux@tutanota.com>
2020-02-23 10:25:50 -0600

    added users routes with dummy data

---
+++ controllers/users-controllers.js
@@ -0,0 +1,56 @@
+const uuid = require('uuid/v4');
+
+const HttpError = require('../models/http-error');
+
+const DUMMY_USERS = [
+	{
+		id: 'u1',
+		name: 'James Hagood',
+		email: 'test@test.com',
+		password: 'testers'
+	}
+];
+
+// @type -- GET
+// @path -- /api/users
+// @desc -- path to get all the users
+const getUsers = (req, res, next) => {
+	res.json({ users: DUMMY_USERS });
+};
+
+// @type -- POST
+// @path -- /api/users/signup
+// @desc -- path to register a new user
+const signup = (req, res, next) => {
+	const { name, email, password } = req.body;
+
+	const createdUser = {
+		id: uuid(),
+		name: name,
+		email: email,
+		password: password
+	};
+	DUMMY_USERS.push(createdUser);
+	res.status(201).json({ user: createdUser });
+};
+
+// @type -- POST
+// @path -- /api/users/login
+// @desc -- path to login a user
+const login = (req, res, next) => {
+	const { email, password } = req.body;
+
+	const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+	if (!identifiedUser || identifiedUser.password !== password) {
+		throw new HttpError(
+			'Could Not Identify User, Credentials Could Be Wrong',
+			401
+		);
+	}
+
+	res.json({ message: 'Logged In!' });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -4,6 +4,7 @@ const placesControllers = require('../controllers/places-controller');
 
 const router = express.Router();
 
+// -----------------Places Routes Starts-----------------------------
 router.get('/:pid', placesControllers.getPlaceById);
 
 router.get('/user/:uid', placesControllers.getPlacesByUserId);
@@ -13,5 +14,6 @@ router.post('/', placesControllers.createPlace);
 router.patch('/:pid', placesControllers.updatePlaceById);
 
 router.delete('/:pid', placesControllers.deletePlaceById);
+// ----------------Places Routes Ends----------------------------------
 
 module.exports = router;
