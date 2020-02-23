9e56d117e5023d78cd62dbf05f7f424f5641287a
James Hagood <cardfaux@tutanota.com>
2020-02-23 10:32:05 -0600

    added check to check if email exists

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -24,6 +24,11 @@ const getUsers = (req, res, next) => {
 const signup = (req, res, next) => {
 	const { name, email, password } = req.body;
 
+	const hasUser = DUMMY_USERS.find((u) => u.email === email);
+	if (hasUser) {
+		throw new HttpError('Could Not Create User, E-Mail Already Exists', 422);
+	}
+
 	const createdUser = {
 		id: uuid(),
 		name: name,
